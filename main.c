af5a7c4a270a5dfd37245dee59aaf10b45fe7fbb
Felipe C. Arreola <arreolaf@oregonstate.edu>
2021-05-23 23:57:06 -0700

    added struct: planet {name, description}, added array of planets, added a few dialogue lines, and a getter for user name

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 // gcc -Wall -g main.c -o main
 // ./main
 
+struct planet
+{
+    char name[20];
+    char description[127];
+};
+
+struct planet arr_planets[9];
+
 int main(int argc, char *argv[])
 {
-    
+    printf("Welcome to the Solar System!\n");
+    printf("There are 9 planets to explore.\n");
+    printf("What is your name?\n");
+    char name[40];
+    fgets(name,40,stdin);
+    name[strcspn(name, "\n")] = 0;
+    printf("Nice to meet you, %s. My name is Eliza\
+    I'm an old frind of Alexa.\n");
+    printf("let's go on an adventure!");
+
     return 0;
 }
