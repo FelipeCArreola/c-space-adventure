bafbf963cda4a912656bc041e877dea7e2b1b093
Felipe C. Arreola <arreolaf@oregonstate.edu>
2021-06-01 14:11:44 -0700

    atempted to add ability to read 'file' and input @ each index of array of planets, added a few functions: 'get_planet_name_entry', 'search_planets_for_name', 'has compiled expression', 'assess'input'

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <regex.h>
 
 // gcc -Wall -g main.c -o main
 // ./main
@@ -42,12 +43,65 @@ int count_of_lines_in_file (FILE *f) {
     return count;
 }
 
+char *get_planet_name_entry(){
+    char *entry = (char *) malloc(20);
+    printf("Name the planet you would like to visit.\n");
+    scanf("%[a-zA-Z]",entry);
+    return entry;
+}
+
+void search_planets_for_name (char *str) {
+    int idx = 0;
+    while (idx < PLANETS_COUNT)
+    {
+        if(strcmp(arr_planets[idx].name, str) == 0) {
+            printf("Traveling to %s...\n", arr_planets[idx].name);
+            printf("Arrived at %s. %s\n", arr_planets[idx].name, arr_planets[idx].description);
+            break;
+        } else if (idx == (PLANETS_COUNT - 1) ) {
+            printf("Traveling to Null...\n");
+            printf("Arrived at Null. Null\n");
+        }
+    }      
+    free(str);
+}
+
+
+void has_compiled_expression (int num) {
+    if (num) {
+        fprintf(stderr, "Regular Expression could not be compiled");
+        exit(1);
+    }
+}
+
+// return 0 for false
+// return 1 for true
+
+int assess_input (char *input, char *regex_str) {
+    regex_t reg_y;
+    int assessed_value = 0;
+    int boolean = 0;
+    // assessed_value = regcomp(&reg_y,"^[Yy]", 0 );
+    assessed_value = regcomp(&reg_y, regex_str, 0 );
+    has_compiled_expression(assessed_value);
+    assessed_value = regexec(&reg_y, input, 0, NULL, 0);
+    if (!assessed_value) {
+        boolean = 1;
+    } else if (assessed_value == REG_NOMATCH) {
+        printf("No Match\n");
+    } else {
+        printf("Error Has Occured-- Regexec: %s, for Regex-Pattern: %s", input, regex_str);
+        exit(1);
+    }
+    return boolean;
+}
+
 int main(int argc, char *argv[])
 {
     FILE *pf;
     char *fPath = "./data/solar_system.txt";
     pf = fopen ( fPath, "r");
-    char line_buffer[150];
+    char *line_buffer= (char *) malloc(150);
     // error handling: 
     if (pf == NULL) {
         fprintf(stderr, "Value of error: %d\n", errno);
@@ -62,17 +116,33 @@ int main(int argc, char *argv[])
         exit(1);
     } 
 
+    // TODO : add ability to read line by line
+    // --- BETTER yet work on cJSON and import- work and parse that
+    // error-- not reading file?? 
+
+    int line_idx = 0;
+    while (fgets(line_buffer, sizeof(line_buffer), pf))
+    {
+        if (line_idx == 0) {
+            arr_planets[line_idx].name = strtok(line_buffer, ":");
+            arr_planets[line_idx].description = strtok(NULL, ":");    
+        } else {
+            arr_planets[line_idx].name = strtok(NULL, ":");
+            arr_planets[line_idx].description = strtok(NULL, ":");
+        }
+        line_idx ++;
+    }
+        
+    fclose(pf);
+
+    
+    // TESTING- printing array values
     for (size_t i = 0; i < PLANETS_COUNT; i++)
     {
-        fgets(line_buffer, sizeof(line_buffer), pf);
-        if (i == 0) {
-            arr_planets[i].name = strtok(line_buffer, ":");
-            arr_planets[i].description = strtok(NULL, ":");
-        } else {
-            arr_planets[i].name = strtok(NULL, ":");
-            arr_planets[i].description = strtok(NULL, ":");
-        }
+        printf("name: %s , description: %s\n", arr_planets[i].name, arr_planets[i].description);
     }
+    
+    
     
     printf("Welcome to the Solar System!\n");
     printf("There are 9 planets to explore.\n");
@@ -88,6 +158,8 @@ int main(int argc, char *argv[])
     playerChoice[strcspn(playerChoice, "\n")] = 0;
     printf("Choice is %s\n", playerChoice);
     // TODO : if clause that assesses 
+    // printf("entry: %s\n", get_planet_name_entry());
+    search_planets_for_name( get_planet_name_entry() );
     printf("Random Number: %d\n", random_planet_idx());
     printf("Traveling to Pluto...\n");
     printf("Arrived at Pluto, it's very cold here.\n");
